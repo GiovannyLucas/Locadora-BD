ebdb9650c18346814ab338559eb6956cd85381e6
GiovannyLucas <geovannylucas2013@outlook.com>
2018-10-25 19:56:54 -0200

    Definição das  classes de MODELO

---
+++ src/Modelo/Cliente.java
@@ -0,0 +1,105 @@
+package Modelo;
+
+public class Cliente {
+
+    private int Codigo;
+    private String duracao;
+    private String Nome;
+    private String Nascimento;
+    private String RG;
+    private String CPF;
+    private String Telefone;
+    private String Email;
+    private String Bairro;
+    private int Numero;
+    private String CEP;
+
+    public int getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(int Codigo) {
+        this.Codigo = Codigo;
+    }
+
+    public String getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(String duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getNascimento() {
+        return Nascimento;
+    }
+
+    public void setNascimento(String Nascimento) {
+        this.Nascimento = Nascimento;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String Telefone) {
+        this.Telefone = Telefone;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getBairro() {
+        return Bairro;
+    }
+
+    public void setBairro(String Bairro) {
+        this.Bairro = Bairro;
+    }
+
+    public int getNumero() {
+        return Numero;
+    }
+
+    public void setNumero(int Numero) {
+        this.Numero = Numero;
+    }
+
+    public String getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(String CEP) {
+        this.CEP = CEP;
+    }
+    
+}
